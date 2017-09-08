49bc488df9436f1b34b3ee1322ef1dd9e9bf63e7
unknown <sruthim07@gmail.com>
2017-09-08 19:50:04 -0400

    first commit

---
+++ src/main/java/com/tek/springmvcumsui/config/AppInitializer.java
@@ -0,0 +1,23 @@
+package com.tek.springmvcumsui.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		System.out.println("UI AppInitializer getRooConfigClasses()");
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/java/com/tek/springmvcumsui/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.tek.springmvcumsui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getIndexPage() {
+		System.out.println("IndexController");
+		return "UserManagement";
+	}
+
+}
